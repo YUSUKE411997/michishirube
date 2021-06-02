1fb87749182624f4de5c38324f34bea0cf4b9675
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-06-02 07:40:00 +0000

    like2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_144811) do
+ActiveRecord::Schema.define(version: 2021_06_01_175459) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2021_06_01_144811) do
     t.integer "user_id", null: false
     t.integer "post_id", null: false
     t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
