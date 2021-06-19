16dd00f6acf786d508cd52af1bdc341761d9f6d8
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-06-19 12:18:24 +0000

    [add]tag bug

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,10 +6,9 @@ class Tag < ApplicationRecord
   # タグのランキング機能（週間ランキング）
   def self.create_ranks_tag
   # tag_week = Tag.where(created_at: 0.days.ago.prev_week..0.days.ago.prev_week(:sunday))
-  # # byebug
+  # byebug
   # tag_week.find(TagMap.group(:tag_id).order('count(tag_id)desc').limit(3).pluck(:tag_id))
 
-  tag_week = Tag.where("Date(created_at) = ?", Date.today)
-  tag_week.find(TagMap.group(:tag_id).order('count(tag_id)desc').limit(3).pluck(:tag_id))
+  tag_day = Tag.find(TagMap.where(created_at: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day).group(:tag_id).order('count(tag_id)desc').limit(3).pluck(:tag_id))
   end
 end
