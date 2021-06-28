c1e0acd24fa22592ea0ed7554a6258041dfdced9
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-06-28 08:23:12 +0000

    [fix]like bug

--- app/controllers/users/likes_controller.rb
+++ app/controllers/users/likes_controller.rb
@@ -3,10 +3,13 @@ class Users::LikesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    like = current_user.likes.new(post_id: @post.id)
-    like.save
+    current_likes = current_user.likes.find_by(post_id: @post.id, user_id: current_user.id)
+    if current_likes.blank?
+      like = current_user.likes.new(post_id: @post.id)
+      like.save
+    end
     @post.create_notification_like!(current_user)
-    
+
   end
 
   def destroy
