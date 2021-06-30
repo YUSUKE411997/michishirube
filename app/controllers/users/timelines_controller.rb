387d6a4b179a3307e92a86844980a41e029235ef
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-06-30 01:15:34 +0000

    [add]

--- app/controllers/users/timelines_controller.rb
+++ app/controllers/users/timelines_controller.rb
@@ -5,6 +5,6 @@ class Users::TimelinesController < ApplicationController
     @timelines = Timeline.timeline_posts(current_user).page(params[:page])
     followings_users_ids = current_user.followings.pluck(:id)
     followings_like_post_ids = Like.where(user_id: followings_users_ids).pluck(:post_id)
-    @follow_like_post = Post.where(id: followings_like_post_ids, created_at: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day).order(created_at: :desc).sample(5)
+    @follow_like_post = Post.where(id: followings_like_post_ids, created_at: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day).sample(5)
   end
 end
